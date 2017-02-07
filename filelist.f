9146a13a1db64978c88497ed001b5bdd6514e06c
Andrei Pazniak <krucios@mail.ru>
2017-02-07 16:12:28 +0300

    [+] servo_fsm module

--- filelist.f
+++ filelist.f
@@ -5,6 +5,7 @@ ${PROJ}/testbench.sv
 ${PROJ}/src/servo_driver.v
 ${PROJ}/src/sonar_driver.v
 ${PROJ}/src/control_unit.v
+${PROJ}/src/servo_fsm.v
 ${PROJ}/src/echo.v
 ${PROJ}/src/sonar_vip.v
 ${PROJ}/src/coreuart_vip.v
